96009b2783932bff84e726f889c3a5aa89b7d017
zhukovny <cruvar.zhukov@yandex.ru>
2023-05-24 19:15:28 +0700

    add add models and migration

--- project/organization/models.py
+++ project/organization/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    director = models.OneToOneField(
+        'Employee',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='directed_department',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='employee_photos')
+    position = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.PositiveIntegerField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
+
+    class Meta:
+        unique_together = ('name', 'department')
+
+    @classmethod
+    def filter_by_last_name(cls, last_name):
+        return cls.objects.filter(name__endswith=last_name)
+
+    def __str__(self):
+        return self.name
