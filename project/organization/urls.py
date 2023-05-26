0516727c2a492ea51034ed9cc0f3a2c9df715cfd
zhukovny <cruvar.zhukov@yandex.ru>
2023-05-26 19:32:52 +0700

    add employees viewset

--- project/organization/urls.py
+++ project/organization/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
 from . import views
+from .views import EmployeeViewSet
+
+
+employee_list = EmployeeViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('employees/', employee_list, name='employee-list'),
 ]
--- project/organization/views.py
+++ project/organization/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from rest_framework import viewsets
+
+from .models import Employee
+from .serializers import EmployeeSerializer
 
 
 def index(request):
     return HttpResponse("Test")
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
